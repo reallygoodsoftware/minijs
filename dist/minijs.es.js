c0546f69b4b92157fd7fd6dcf37eb75a311cf67c
Jen <jengkarlong@gmail.com>
2023-08-15 15:37:33 +0800

    Add each function

--- dist/minijs.es.js
+++ dist/minijs.es.js
@@ -5,15 +5,15 @@ class Entity {
     };
   }
   get variables() {
-    const e = MiniJS.variables, t = Array.from(this.element.attributes).map((a) => a.value).join(" ");
-    return e.filter((a) => t.includes(a));
+    const e = MiniJS.variables, t = Array.from(this.element.attributes).map((l) => l.value).join(" ");
+    return e.filter((l) => t.includes(l));
   }
   get baseClasses() {
     return this.initialState.className.split(" ");
   }
   _eventAction(e) {
     const t = this.element.getAttribute(e);
-    return this._sanitizeExpression(t);
+    return console.log(t), this._sanitizeExpression(t);
   }
   _sanitizeExpression(e) {
     return this.variables.forEach((t) => {
@@ -29,6 +29,32 @@ class Entity {
       const newClassNames = eval(classExpr), classesCombined = [...this.baseClasses, ...newClassNames.split(" ")].join(" ");
       this.element.className = classesCombined;
     }
+  }
+  evaluateEach() {
+    const eachExpr = this.element.getAttribute(":each");
+    if (eachExpr) {
+      const [variable, iterable] = eachExpr.split(" in "), items = eval(iterable);
+      this.childClone || (this.childClone = this.element.innerHTML);
+      let newHTML = "";
+      items.forEach((e) => {
+        newHTML += this.childClone.replaceAll(variable, `'${e}'`);
+      }), this.element.innerHTML = newHTML;
+      const xpathResult = document.evaluate(
+        './/*[@*[starts-with(name(), ":")]]',
+        this.element,
+        // Use the current :each element as the context node
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+      );
+      for (let e = 0; e < xpathResult.snapshotLength; e++) {
+        const t = new Entity(xpathResult.snapshotItem(e));
+        t.applyEventBindings(), t.evaluateAll();
+      }
+    }
+  }
+  evaluateAll() {
+    this.evaluateValue(), this.evaluateClass(), this.evaluateText();
   }
   evaluateText() {
     const textExpr = this.element.getAttribute(":text");
@@ -49,6 +75,20 @@ class Entity {
       newValue && (this.element.checked = newValue);
     }
   }
+  applyEventBindings() {
+    const e = this.element;
+    e.hasAttribute(":click") && e.addEventListener("click", () => {
+      this.evaluateEventAction(":click");
+    }), e.hasAttribute(":change") && (e.type == "checkbox" || e.tagName == "select" ? e.addEventListener("change", () => {
+      this.evaluateEventAction(":change");
+    }) : e.addEventListener("input", () => {
+      this.evaluateEventAction(":change");
+    })), e.hasAttribute(":enter") && e.addEventListener("keypress", (t) => {
+      t.key == "Enter" && this.evaluateEventAction(":enter");
+    }), document.addEventListener("click", (t) => {
+      e.hasAttribute(":clickout") && !e.contains(t.target) && this.evaluateEventAction(":clickout");
+    });
+  }
   hasAttribute(e) {
     return !!this.element.getAttribute(e);
   }
@@ -61,7 +101,30 @@ const MiniJS$1 = (() => {
   };
   window.proxyWindow = null;
   async function init() {
-    await _domReady(), _findElements(), _initializeGlobalVariables(), _setProxyWindow(), _applyBindings(), updateStates();
+    await _domReady(), _findElements(), _initializeGlobalVariables(), _setProxyWindow(), _addMethodsToVariables(), _applyBindings(), updateStates();
+  }
+  function _addMethodsToVariables() {
+    _variables.forEach((e) => {
+      if (Array.isArray(proxyWindow[e])) {
+        let r = function(a) {
+          let i;
+          if (Array.isArray(a))
+            i = a;
+          else {
+            let s = this.indexOf(a);
+            s === -1 ? i = this.concat(a) : i = this.slice(0, s).concat(this.slice(s + 1));
+          }
+          proxyWindow[e] = i, proxyWindow[e].toggle = r;
+        }, o = function(a, i) {
+          const s = a.toLowerCase(), h = i.filter((c) => c.toLowerCase().includes(s));
+          return h.sort((c, d) => Math.abs(s.length - c.length) - Math.abs(s.length - d.length)), h;
+        }, u = function(a) {
+          return o(a, this);
+        };
+        var t = r, n = o, l = u;
+        proxyWindow[e].search = u, proxyWindow[e].toggle = r;
+      }
+    });
   }
   function _setProxyWindow() {
     proxyWindow = new Proxy(window, watchHandler);
@@ -70,8 +133,11 @@ const MiniJS$1 = (() => {
     _elements.forEach((entity) => {
       const el = entity.element, loadExpr = el.getAttribute(_loadEvent);
       if (loadExpr) {
-        const [varName, varVal] = loadExpr.replace(" ", "").split("=");
-        window[varName] = varName.startsWith("$") && JSON.parse(localStorage.getItem(varName)) || void 0, eval(loadExpr), _variables.push(varName);
+        const assignments = loadExpr.split(";").map((e) => e.trim()).filter((e) => e.length > 0);
+        assignments.forEach((assignment) => {
+          const [varName, varVal] = assignment.replace(" ", "").split("=");
+          window[varName] = varName.startsWith("$") && JSON.parse(localStorage.getItem(varName)) || void 0, eval(assignment), _variables.push(varName);
+        });
       } else
         _actionEvents.forEach((e) => {
           const t = el.getAttribute(e);
@@ -84,24 +150,12 @@ const MiniJS$1 = (() => {
   }
   function updateStates(e = null) {
     _elements.forEach((t) => {
-      (t.variables.includes(e) || e == null) && (t.evaluateValue(), t.evaluateClass(), t.evaluateText());
+      (t.variables.includes(e) || e == null) && (t.evaluateEach(), t.evaluateAll());
     });
-  }
-  function triggerEventChanges(e, t) {
-    e.evaluateEventAction(t);
   }
   function _applyBindings() {
     _elements.forEach((e) => {
-      const t = e.element;
-      t.hasAttribute(":click") && t.addEventListener("click", () => {
-        triggerEventChanges(e, ":click");
-      }), t.hasAttribute(":change") && (t.type == "checkbox" || t.tagName == "select" ? t.addEventListener("change", () => {
-        triggerEventChanges(e, ":change");
-      }) : t.addEventListener("input", () => {
-        triggerEventChanges(e, ":change");
-      })), document.addEventListener("click", function(n) {
-        e.hasAttribute(":clickout") && !e.element.contains(n.target) && e.evaluateEventAction(":clickout");
-      });
+      e.applyEventBindings();
     });
   }
   function _findElements() {
@@ -114,8 +168,16 @@ const MiniJS$1 = (() => {
     );
     for (let t = 0; t < e.snapshotLength; t++) {
       const n = new Entity(e.snapshotItem(t));
-      _elements.push(n);
+      isInsideEachElement(n.element.parentElement) || _elements.push(n);
     }
+  }
+  function isInsideEachElement(e) {
+    for (; e; ) {
+      if (e.hasAttribute && e.hasAttribute(":each"))
+        return !0;
+      e = e.parentElement;
+    }
+    return !1;
   }
   function _domReady() {
     return new Promise((e) => {
